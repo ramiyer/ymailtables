5b89219b7ca67d94c846de9b601b049d4620aed3
ramn <yramiyer@yahoo.com>
2010-07-07 14:16:59 -0700

    cleanup

--- messages.js
+++ messages.js
@@ -55,8 +55,6 @@ function buildMsgParams(){
 			
 	for(i=0; i<mids.length; i++) {
 		message.push({
-			"restrictCSS" : inputs['restrictCSS'] ,
-			"blockImages" : inputs['blockImages'] ,
 			"mid": mids[i].toString()
 	 	});
 	}
